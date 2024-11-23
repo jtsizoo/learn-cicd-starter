e1ccb064a25b895c39cabb71353a8f0d128b4cca
Justin <sizoo.justin@gmail.com>
2024-11-22 18:39:44 -0600

    add unit test to pipeline

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,13 @@
+package auth
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	key, err := GetAPIKey(http.Header{})
+	if key != ""  && err != ErrNoAuthHeaderIncluded {
+		t.Errorf("empty header should return blank key")
+	}
+}
