781b2632d513c56a52d2121db3e0cdd0e81ad7bc
Justin <sizoo.justin@gmail.com>
2024-12-06 20:43:19 -0600

    fix formatting

--- main.go
+++ main.go
@@ -89,9 +89,9 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
-		ReadTimeout: 1000,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadTimeout:       1000,
 		ReadHeaderTimeout: 0,
 	}
 
